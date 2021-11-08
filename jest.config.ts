a7c42fdd2e6b353efed876d6b1805a6dab58ff54
Iuri Severo <nobreiuri@gmail.com>
2021-11-08 17:47:30 -0300

    Resolve ESLint warnings and errors

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,16 @@ export default {
   clearMocks: true,
   coverageProvider: 'v8',
   preset: 'ts-jest',
-  reporters: ['default',  ['jest-sonar', {
-    outputDirectory: 'coverage',
-    outputName: 'test-report.xml',
-    reportedFilePath: 'relative'
-  }]],
+  reporters: [
+    'default',
+    [
+      'jest-sonar',
+      {
+        outputDirectory: 'coverage',
+        outputName: 'test-report.xml',
+        reportedFilePath: 'relative',
+      },
+    ],
+  ],
   testMatch: ['**/**/*.spec.ts'],
-  
 };
